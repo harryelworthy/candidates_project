b15e942b876caddf78f8d77c19681c04f0622063
Harry Elworthy <harryelworthy@gmail.com>
2019-03-15 17:47:03 -0400

    once again

--- app.py
+++ app.py
@@ -73,4 +73,5 @@ def show_table():
     titles = ['Chances candidates win given they are nominated, based on PredictIt prices'])
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run(    host=os.getenv('LISTEN', '0.0.0.0'),
+    port=int(os.getenv('PORT', '8080')))
