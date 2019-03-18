9c271f0cde7b029ab6b5b2394b9700fa0be7395b
Harry Elworthy <harryelworthy@gmail.com>
2019-03-18 16:35:41 -0400

    trying stuff with flask-apscheduler

--- candidates/__init__.py
+++ candidates/__init__.py
@@ -3,6 +3,132 @@ import os
 from flask import Flask
 from . import db
 
+from flask_apscheduler import APScheduler
+
+from flask import *
+import pandas as pd
+import requests as req
+
+from .db import get_db
+#from .update_probs import add_to_db
+
+import datetime
+import click
+
+from flask.cli import with_appcontext
+
+#from apscheduler.schedulers.background import BackgroundScheduler
+
+BASE_URL = "https://www.predictit.org/api/marketdata/markets/"
+DEM_NOM_ID = 3633
+REP_NOM_ID = 3653
+PRES_ID = 3698
+PARTY_ID = 2721
+
+HEADERS = {'Content-Type':'application/json'}
+PARAMETERS = {'id': str(id), 'responseType': 'JSON'}
+
+
+
+
+class Config(object):
+    JOBS = [
+        {
+            'id': 'update_probs',
+            'func': '__init__:update_probs',
+            'args': (),
+            'trigger': 'interval',
+            'hours': 1
+        }
+    ]
+
+    SCHEDULER_API_ENABLED = True
+
+
+
+def request_api(id):
+    # Make request
+    response = req.get(url=BASE_URL + str(id), json=PARAMETERS, headers=HEADERS)
+    df = pd.DataFrame(response.json())
+    df = pd.concat([df.drop(['contracts', 'shortName', 'name', 'image'], axis=1), df['contracts'].apply(pd.Series)], axis=1)
+    return df
+
+def get_current_cands():
+    # Get presidential candidate list, add new columns with defaults
+    df = request_api(PRES_ID)[['name','image','lastTradePrice']].rename(columns={'lastTradePrice':'presidentPrice'})
+    df['dem'] = False
+    df['rep'] = False
+    df['nominationPrice'] = None
+
+    # Get party candidates
+    dem_candidates = request_api(DEM_NOM_ID)[['name','image','lastTradePrice']].rename(columns={'lastTradePrice':'nominationPrice'})
+    rep_candidates = request_api(REP_NOM_ID)[['name','image','lastTradePrice']].rename(columns={'lastTradePrice':'nominationPrice'})
+
+    # Loop through each, merge in nomination price and set party
+    for name in df['name']:
+        if name in dem_candidates['name'].unique():
+            df.loc[df['name'] == name,'dem'] = True
+            df.loc[df['name'] == name,'nominationPrice'] = dem_candidates.loc[dem_candidates['name'] == name,'nominationPrice'].unique()
+
+    for name in df['name']:
+        if name in rep_candidates['name'].unique():
+            df.loc[df['name'] == name,'rep'] = True
+            df.loc[df['name'] == name,'nominationPrice'] = rep_candidates.loc[rep_candidates['name'] == name,'nominationPrice'].unique()
+
+    # Calculate conditional probability
+    df['conditionalProbability'] = df['presidentPrice']/df['nominationPrice']
+
+    return df
+
+#@click.command('update-probs')
+#@with_appcontext
+#@cron.interval_schedule(hours=1)
+def update_probs():
+    add_to_db()
+    return
+
+
+
+def add_to_db():
+    db = get_db()
+    dt = datetime.datetime.now()
+    error = None
+    df = get_current_cands()
+    if error is None:
+        for index, c in df.iterrows():
+            db.execute(
+                'INSERT INTO probability (dt, name, democrat, Pnom, Ppres, Pcond) VALUES (?, ?, ?, ?, ?, ?)',
+                    (dt,
+                    c['name'],
+                    c['dem'],
+                    c['nominationPrice'],
+                    c['presidentPrice'],
+                    c['conditionalProbability'])
+            )
+    db.commit()
+    return
+#    click.echo('Updated probabilities')
+
+
+
+
+def get_most_recent():
+    db = get_db()
+    res = db.execute("""
+        SELECT
+          name,
+          Pcond,
+          Ppres,
+          democrat
+        FROM
+          probability AS [data]
+        WHERE
+          dt = (SELECT MAX(dt) FROM probability)
+    """).fetchall()
+    df = pd.DataFrame(res)
+    return df
+
+
 
 def create_app(test_config=None):
     # create and configure the app
@@ -24,6 +150,12 @@ def create_app(test_config=None):
         os.makedirs(app.instance_path)
     except OSError:
         pass
+
+    scheduler = APScheduler()
+    # it is also possible to enable the API directly
+    # scheduler.api_enabled = True
+    scheduler.init_app(app)
+    scheduler.start()
 
     from . import db
     db.init_app(app)
--- candidates/table.py
+++ candidates/table.py
@@ -4,10 +4,101 @@ import requests as req
 import os
 
 from .db import get_db
+#from .update_probs import add_to_db
+
+import datetime
+import click
+
+from flask.cli import with_appcontext
+
+#from apscheduler.schedulers.background import BackgroundScheduler
+
+BASE_URL = "https://www.predictit.org/api/marketdata/markets/"
+DEM_NOM_ID = 3633
+REP_NOM_ID = 3653
+PRES_ID = 3698
+PARTY_ID = 2721
+
+HEADERS = {'Content-Type':'application/json'}
+PARAMETERS = {'id': str(id), 'responseType': 'JSON'}
 
 # from .update_probs import get_current_cands
 
 bp = Blueprint('table', __name__, url_prefix='/')
+
+
+
+
+#cron = Scheduler(daemon=True)
+# Explicitly kick off the background thread
+#cron.start()
+
+
+def request_api(id):
+    # Make request
+    response = req.get(url=BASE_URL + str(id), json=PARAMETERS, headers=HEADERS)
+    df = pd.DataFrame(response.json())
+    df = pd.concat([df.drop(['contracts', 'shortName', 'name', 'image'], axis=1), df['contracts'].apply(pd.Series)], axis=1)
+    return df
+
+def get_current_cands():
+    # Get presidential candidate list, add new columns with defaults
+    df = request_api(PRES_ID)[['name','image','lastTradePrice']].rename(columns={'lastTradePrice':'presidentPrice'})
+    df['dem'] = False
+    df['rep'] = False
+    df['nominationPrice'] = None
+
+    # Get party candidates
+    dem_candidates = request_api(DEM_NOM_ID)[['name','image','lastTradePrice']].rename(columns={'lastTradePrice':'nominationPrice'})
+    rep_candidates = request_api(REP_NOM_ID)[['name','image','lastTradePrice']].rename(columns={'lastTradePrice':'nominationPrice'})
+
+    # Loop through each, merge in nomination price and set party
+    for name in df['name']:
+        if name in dem_candidates['name'].unique():
+            df.loc[df['name'] == name,'dem'] = True
+            df.loc[df['name'] == name,'nominationPrice'] = dem_candidates.loc[dem_candidates['name'] == name,'nominationPrice'].unique()
+
+    for name in df['name']:
+        if name in rep_candidates['name'].unique():
+            df.loc[df['name'] == name,'rep'] = True
+            df.loc[df['name'] == name,'nominationPrice'] = rep_candidates.loc[rep_candidates['name'] == name,'nominationPrice'].unique()
+
+    # Calculate conditional probability
+    df['conditionalProbability'] = df['presidentPrice']/df['nominationPrice']
+
+    return df
+
+#@click.command('update-probs')
+#@with_appcontext
+#@cron.interval_schedule(hours=1)
+def update_probs():
+    add_to_db()
+    return
+
+
+
+def add_to_db():
+    db = get_db()
+    dt = datetime.datetime.now()
+    error = None
+    df = get_current_cands()
+    if error is None:
+        for index, c in df.iterrows():
+            db.execute(
+                'INSERT INTO probability (dt, name, democrat, Pnom, Ppres, Pcond) VALUES (?, ?, ?, ?, ?, ?)',
+                    (dt,
+                    c['name'],
+                    c['dem'],
+                    c['nominationPrice'],
+                    c['presidentPrice'],
+                    c['conditionalProbability'])
+            )
+    db.commit()
+    return
+#    click.echo('Updated probabilities')
+
+
+
 
 def get_most_recent():
     db = get_db()
@@ -58,9 +149,13 @@ def html_rep_table():
 
 @bp.route("/")
 def show_table():
+    #add_to_db()
     return render_template('view.html',tables=[html_dem_table(),html_rep_table()],
     titles = ['x','Democrats','Republicans'])
 
 @bp.route("/about")
 def show_about():
     return render_template('about.html')
+
+# Shutdown your cron thread if the web process is stopped
+#atexit.register(lambda: cron.shutdown(wait=False))
